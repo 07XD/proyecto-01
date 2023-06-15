43d4f733c0a283498067d973d02ca7be5c828d9f
Jordy Imanol <65522162+07XD@users.noreply.github.com>
2023-06-14 19:21:47 -0500

    actualización

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-const switchInput = document.querySelector('.switch input');
-const darkIcon = document.querySelector('.dark');
-const lightIcon = document.querySelector('.light');
+const switchInputs = document.querySelectorAll('.switch input');
+const darkIcons = document.querySelectorAll('.dark');
+const lightIcons = document.querySelectorAll('.light');
 const body = document.querySelector('body');
 
 // Función para guardar el tema seleccionado en localStorage
@@ -11,47 +11,78 @@ const saveThemePreference = (theme) => {
 // Función para cargar el tema almacenado en localStorage
 const loadThemePreference = () => {
   const savedTheme = localStorage.getItem('theme');
-  if (savedTheme === 'dark') {
-    body.classList.add('dark-theme');
-    darkIcon.classList.add('active');
-    lightIcon.style.display = 'flex';
-    darkIcon.style.display = 'none';
-    switchInput.checked = true;
+
+  if (savedTheme) {
+    body.classList.remove('dark-theme', 'light-theme');
+    body.classList.add(savedTheme + '-theme');
+    for (let i = 0; i < darkIcons.length; i++) {
+      darkIcons[i].style.display = savedTheme === 'dark' ? 'none' : 'flex';
+    }
+    for (let i = 0; i < lightIcons.length; i++) {
+      lightIcons[i].style.display = savedTheme === 'light' ? 'none' : 'flex';
+    }
+    for (let i = 0; i < switchInputs.length; i++) {
+      switchInputs[i].checked = savedTheme === 'dark';
+    }
   } else {
-    body.classList.add('light-theme');
-    lightIcon.classList.add('active');
-    darkIcon.style.display = 'flex';
-    lightIcon.style.display = 'none';
-    switchInput.checked = false;
+    const prefersDarkTheme = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+    if (prefersDarkTheme) {
+      body.classList.add('dark-theme');
+      saveThemePreference('dark');
+    } else {
+      body.classList.add('light-theme');
+      saveThemePreference('light');
+    }
   }
 };
-
-// Detectar tema del dispositivo
-const prefersDarkTheme = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-
-// Aplicar tema inicial
-if (prefersDarkTheme) {
-  saveThemePreference('dark');
-} else {
-  saveThemePreference('light');
-}
 
 // Cargar tema almacenado
 loadThemePreference();
 
-// Cambiar tema al cambiar el estado del checkbox
-switchInput.addEventListener('change', () => {
-  if (switchInput.checked) {
-    body.classList.remove('light-theme');
-    body.classList.add('dark-theme');
-    lightIcon.style.display = 'flex';
-    darkIcon.style.display = 'none';
-    saveThemePreference('dark');
-  } else {
-    body.classList.remove('dark-theme');
-    body.classList.add('light-theme');
-    darkIcon.style.display = 'flex';
-    lightIcon.style.display = 'none';
-    saveThemePreference('light');
+// Cambiar tema al cambiar el estado del checkbox o al hacer clic en el icono
+const changeTheme = (checked) => {
+  const userTheme = checked ? 'dark' : 'light';
+
+  body.classList.remove('dark-theme', 'light-theme');
+  body.classList.add(userTheme + '-theme');
+  for (let i = 0; i < darkIcons.length; i++) {
+    darkIcons[i].style.display = userTheme === 'dark' ? 'none' : 'flex';
   }
-});
+  for (let i = 0; i < lightIcons.length; i++) {
+    lightIcons[i].style.display = userTheme === 'light' ? 'none' : 'flex';
+  }
+  saveThemePreference(userTheme);
+};
+
+// Actualizar estado del botón en ambas barras al cambiar el tema
+for (let i = 0; i < switchInputs.length; i++) {
+  switchInputs[i].addEventListener('change', () => {
+    const checked = switchInputs[i].checked;
+
+    // Actualizar estado del botón en ambas barras
+    for (let j = 0; j < switchInputs.length; j++) {
+      switchInputs[j].checked = checked;
+    }
+
+    changeTheme(checked);
+  });
+}
+
+// Agregar evento de clic al icono para cambiar el tema
+const darkIconsArray = Array.from(darkIcons);
+const lightIconsArray = Array.from(lightIcons);
+const iconsArray = darkIconsArray.concat(lightIconsArray);
+
+for (let i = 0; i < iconsArray.length; i++) {
+  iconsArray[i].addEventListener('click', () => {
+    const checked = !switchInputs[0].checked;
+
+    // Actualizar estado del botón en ambas barras
+    for (let j = 0; j < switchInputs.length; j++) {
+      switchInputs[j].checked = checked;
+    }
+
+    changeTheme(checked);
+  });
+}
